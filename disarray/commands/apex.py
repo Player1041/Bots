232c53a5f0ded8c738e552d9c476d0f471f90a44
Player1041 <redacteddoesit@gmail.com>
2022-04-13 00:16:15 +0100

    owo

--- disarray/commands/apex.py
+++ disarray/commands/apex.py
@@ -1,125 +1,95 @@
 import disnake
 from disnake.ext import commands
+import aiohttp
 import os
 from dotenv import load_dotenv
-import aiohttp
 import json
 from enum import Enum
 
-class Platform(Enum):
-    PSN = "PS4"
-    PC = "PC"
-    XBOX = "X1"
+load_dotenv()
+apex_auth = os.environ["apex_auth"]
 
-Maps = commands.option_enum({
-    "Arenas": "arenas",
-    "Battle Royale": "battle_royale",
-    "Ranked Arenas": "arenasRanked",
-    "Ranked Battle Royale": "ranked"
+platforms = commands.option_enum({
+    "PSN": "PS4",
+    "PC": "PC",
+    "XBOX": "X1"
 })
 
-load_dotenv()
-
-apex_key = os.environ["apex_key"]
+maps = commands.option_enum({
+    "Ranked Battle Royale": "ranked",
+    "Ranked Arenas": "arenasRanked",
+    "Battle Royale": "battle_royale",
+    "Arenas": "arenas"
+})
 
 class Apex(commands.Cog):
-    """A list of apex commands."""
     
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        
+    
     @commands.slash_command()
     async def apex(self, inter):
         pass
     
     @apex.sub_command()
-    async def stats(self, inter, platform: Platform, user: str):
-        """Shows the stats of a given user.
-        
-        Parameters
-        ----------
-        
-        platform: Select from PC, PSN or Xbox, there is no switch support due to API limitations.
-        user: The username of the user to search"""
-
-        async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://api.mozambiquehe.re/bridge?version=5&platform={platform}&player={user}&auth={apex_key}") as resp:
+    async def maps(self, inter, mode: maps):
+        async with aiohttp.ClientSession() as sesh:
+            async with sesh.get(f"https://api.mozambiquehe.re/maprotation?version=5&auth={apex_auth}") as resp:
                 data = json.loads(await resp.text())
+                mapEmbed = disnake.Embed(
+                    title = f"{maps(mode).name}!",
+                    description = f"The current map for {maps(mode).name} is {data[mode]['current']['map'].title()}."
+                ).set_image(
+                    url = data[mode]['current']['asset']
+                )
+                    
+        await inter.response.send_message(embed = mapEmbed)
+        
+    @apex.sub_command()
+    async def predators(self, inter):
+        async with aiohttp.ClientSession() as sesh:
+            async with sesh.get(f"https://api.mozambiquehe.re/predator?version=5&auth={apex_auth}") as resp:
+                data = json.loads(await resp.text())
+                predEmbed = disnake.Embed(
+                    title = "Predator Requirements!"
+                    ).add_field(
+                        name = "Battle Royale:",
+                        value = 
+                        f"PC: {data['RP']['PC']['val']} ; Total Masters + Preds: {data['RP']['PC']['totalMastersAndPreds']}\n"
+                        f"PSN: {data['RP']['PS4']['val']} ; Total Masters + Preds: {data['RP']['PS4']['totalMastersAndPreds']}\n"
+                        f"XBOX: {data['RP']['X1']['val']} ; Total Masters + Preds: {data['RP']['X1']['totalMastersAndPreds']}\n"
+                        f"Switch: {data['RP']['SWITCH']['val']} ; Total Masters + Preds: {data['RP']['SWITCH']['totalMastersAndPreds']}"
+                    ).add_field(
+                        name = "Arenas:",
+                        value = 
+                        f"PC: {data['AP']['PC']['val']} ; Total Masters + Preds: {data['AP']['PC']['totalMastersAndPreds']}\n"
+                        f"PSN: {data['AP']['PS4']['val']} ; Total Masters + Preds: {data['AP']['PS4']['totalMastersAndPreds']}\n"
+                        f"XBOX: {data['AP']['X1']['val']} ; Total Masters + Preds: {data['AP']['X1']['totalMastersAndPreds']}\n"
+                        f"Switch: {data['AP']['SWITCH']['val']} ; Total Masters + Preds: {data['AP']['SWITCH']['totalMastersAndPreds']}"
+                        )
+        await inter.response.send_message(embed = predEmbed)
+    
+    @apex.sub_command()
+    async def stats(self, inter, user: str, platform: platforms):
+        async with aiohttp.ClientSession() as sesh:
+            async with sesh.get(f"https://api.mozambiquehe.re/bridge?version=5&platform={platform}&player={user}&auth={apex_auth}") as resp:
+                data = json.loads(await resp.text())
+                print(data)
                 statsEmbed = disnake.Embed(
-                    title = "Player Stats - Apex",
-                    description = f"{data['global']['name']}'s info:",
-                    colour = disnake.Colour.random()
+                    title = f"Stats for {data['global']['name']}"
                 ).add_field(
-                        name = "General:",
-                        value = f"""UID - {data['global']['uid']}
-                        \nLevel - {data['global']['level']}
-                        """
+                    name = "General Stats:",
+                    value =
+                        f"Name: {data['global']['name']}\n"
+                        f"Last Platform: {data['global']['platform']}\n"
+                        f"Banned: {data['global']['bans']['isActive']}\n"
                 ).add_field(
-                        name = "Ranks:",
-                        value = f"""Battle Royale - {data['global']['rank']['rankName']} {data['global']['rank']['rankDiv']}
-                        \nRP - {data['global']['rank']['rankScore']}
-                        \nArena - {data['global']['arena']['rankName']} {data['global']['arena']['rankDiv']}
-                        \nAP - {data['global']['arena']['rankScore']}""")
-                await inter.response.send_message(embed = statsEmbed)
+                    name = "Ranks + RP/AP this split:",
+                    value = f"Battle Royale: {data['global']['rank']['rankName']} {data['global']['rank']['rankDiv']}  - {data['global']['rank']['rankScore']} RP\n"
+                    f"Arenas: {data['global']['arena']['rankName']} {data['global']['arena']['rankDiv']} - {data['global']['arena']['rankScore']} AP"
+                    )
+        await inter.response.send_message(embed = statsEmbed)
 
-
-    @apex.sub_command()
-    async def maps(self, inter, mode: Maps):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://api.mozambiquehe.re/maprotation?version=5&auth={apex_key}") as resp:
-                data = json.loads(await resp.text())
-
-                arenaEmbed = disnake.Embed(
-                    title = "Arenas",
-                    description = f"The current map for Arenas is {data['arenas']['current']['map']}."
-                ).set_image(
-                    url = data['arenas']['current']['asset']
-                )
-
-                
-                rankedArenaEmbed = disnake.Embed(
-                    title = "Ranked Arenas",
-                    description = f"The current map for Ranked Arenas is {data['arenasRanked']['current']['map']}."
-                ).set_image(
-                    url = data['arenasRanked']['current']['asset']
-                )
-
-                
-                battleRoyaleEmbed = disnake.Embed(
-                    title = "Battle Royale",
-                    description = f"The current map for Battle Royale is {data['battle_royale']['current']['map']}."
-                ).set_image(
-                    url = data['battle_royale']['current']['asset']
-                )
-
-                
-                rankedBREmbed = disnake.Embed(
-                    title = "Ranked Battle Royale",
-                    description = f"The current map for Ranked Battle Royale is {data['ranked']['current']['map']}."
-                ).set_image(
-                    url = data['ranked']['current']['asset']
-                )
-
-                
-                if mode == "arenas":
-                    await inter.response.send_message(embed = arenaEmbed)
-                elif mode == "arenasRanked":
-                    await inter.response.send_message(embed = rankedArenaEmbed)
-                elif mode == "battle_royale":
-                    await inter.response.send_message(embed = battleRoyaleEmbed)
-                elif mode == "ranked":
-                    await inter.response.send_message(embed = rankedBREmbed)
-
-    @apex.sub_command()
-    async def crafting(self, inter):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://api.mozambiquehe.re/crafting?&auth={apex_key}") as resp:
-                data = json.loads(await resp.text())
-                craftingEmbed = disnake.Embed(
-                    name = "Crafting Rotations",
-                    description = f"""The next rotation is at <t:"""
-                )
-                
 def setup(bot):
     bot.add_cog(Apex(bot))
-    print("| Loaded the Apex Commands")
+    print("Apex Loaded.")
