70f94c9bbc6958b2150c78232100cb0f54ccae08
Player <redacteddoesit@gmail.com>
2022-04-15 18:29:24 +0100

    Update role.py

--- yamb/commands/role.py
+++ yamb/commands/role.py
@@ -28,10 +28,29 @@ class Roles(commands.Cog):
             description = f"{role.mention} was assigned to {member.mention} / {member} by {inter.author.mention} / {inter.author}."
             )
         failHeirEmbed = disnake.Embed(
-            title = "Role failed to assign: Heirarchy Error",
-            description = f"You ({inter.author.mention} / {inter.author}) have a role lower than {role.mention}. Get a higher role and try again.")
+            title = "Role failed to assign: Hierarchy Error",
+            description = f"You ({inter.author.mention} / {inter.author}) have a role lower than {role.mention}. Get a higher role and try again."
+            )
+        botRoleEmbed = disnake.Embed(
+            title = "Role failed to assign: Bot Role",
+            description = f"You ({inter.author.mention} / {inter.author}) have tried to assign a bot managed role. Select a different role and try again."
+            )
+        selfPermsEmbed = disnake.Embed(
+            title = "Role failed to assign: Missing Permissions",
+            description = f"You ({inter.author.mention} / {inter.author}) have not got the `Manage Roles` permission. Get the `Manage Roles` permission and try again."
+            )
+        botPermsEmbed = disnake.Embed(
+            title = "Role failed to assign: Missing Permissions",
+            description = f"I have not got the `Manage Roles` permission. Give me the `Manage Roles` permission and try again."
+            )
+        if not inter.guild.me.guild_permissions.manage_roles:
+            return await inter.response.send_message(embed = botPermsEmbed)
+        if not inter.author.guild_permissions.manage_roles:
+            return await inter.response.send_message(embed = selfPermsEmbed)
         if inter.author.top_role < role:
-            await inter.response.send_message(embed = failHeirEmbed)
+            return await inter.response.send_message(embed = failHeirEmbed)
+        if not role.is_assignable():
+            return await inter.response.send_message(embed = botRoleEmbed)
         await member.add_roles(role, reason=f": {reason}")
         await inter.response.send_message(embed = roleEmbed)
     
